63dde6b6eaaa4ff6d76bec3c0d448bec82cd5335
a.b <nion8@mail.ru>
2018-01-19 15:51:58 +0300

    джанго 1.11-3

--- posts/forms.py
+++ posts/forms.py
@@ -2,8 +2,11 @@ from django import forms
 from .models import *
 
 
+#
+# class PostForm(forms.ModelForm):
+#     class Meta:
+#         model = Post
+#         exclude = [""]
 
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        exclude = [""]
+class PostForm(forms.Form):
+    text = forms.CharField(widget=forms.Textarea(attrs={'rows: 2', 'cols: 80'}), max_length=300)
