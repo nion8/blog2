40f30590a45b7744f8fcda6592b4aa7a233ee4b5
a.b <nion8@mail.ru>
2018-01-20 15:27:36 +0300

    forms класс postpost

--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,7 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 from django.views import View
-from .models import Post
+from .models import Post, HashTag
 from user_profile.models import User
 from posts.forms import PostForm
 
@@ -25,12 +25,24 @@ class Profile(View):
     def get(self, request, username):
         user = User.objects.get(username=username)
         posts = Post.objects.filter(user=user)
-        form = PostForm()
         context = {
             'posts': posts,
             'user': user,
-            'form': form,
+
         }
         return render(request, 'profile.html', context)
+
+
+class PostPost(View):
+    def post(self, request, usermane):
+        form = PostForm(self.request.POST)
+        if form.is_valid():
+            user = User.objects.get(usermane=usermane)
+            post = Post(text=form.cleaned_data['text'], user = user)
+            post.save()
+            words = form.cleaned_data('text'.split(" "))
+            for word in words:
+                if word.startswith('#'):
+                    hash_tag, created = HashTag.objects.get_or_create(name=word)
 
 
