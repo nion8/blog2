ba6998a1d6e4615aac7e9260f5d6645a8bc909bb
a.b <nion8@mail.ru>
2018-01-20 16:02:35 +0300

    forms класс postpost username

--- posts/views.py
+++ posts/views.py
@@ -26,26 +26,26 @@ class Profile(View):
     def get(self, request, username):
         user = User.objects.get(username=username)
         posts = Post.objects.filter(user=user)
+        form = PostForm()
         context = {
             'posts': posts,
             'user': user,
+            'form': form
 
         }
         return render(request, 'profile.html', context)
 
 
 class PostPost(View):
-    def post(self, request, usermane):
+    def post(self, request, username):
         form = PostForm(self.request.POST)
         if form.is_valid():
-            user = User.objects.get(usermane=usermane)
-            post = Post(text=form.cleaned_data['text'], user = user)
+            user = User.objects.get(username=username)
+            post = Post(text=form.cleaned_data['text'], user=user)
             post.save()
-            words = form.cleaned_data('text'.split(" "))
+            words = form.cleaned_data['text'].split(" ")
             for word in words:
                 if word.startswith('#'):
                     hash_tag, created = HashTag.objects.get_or_create(name=word)
                     hash_tag.post.add(post)
-        return HttpResponseRedirect('/user/'+usermane)
-
-
+        return HttpResponseRedirect('/user/' + username)
